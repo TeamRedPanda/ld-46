2d4d0e30ebf11b80773ca9b14652f82653bb89da
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2020-04-19 18:13:04 -0300

    Add firing controller for turret

---
+++ Assets/Scripts/Turret/FiringController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiringController : MonoBehaviour
+{
+    public GameObject m_BulletPrefab;
+    public Transform m_Target;
+    public Transform m_SpawnPoint;
+
+    [SerializeField] float m_FirePeriod;
+    float m_TimeSinceLastShot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_TimeSinceLastShot = m_FirePeriod;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_TimeSinceLastShot += Time.deltaTime;
+        if (m_TimeSinceLastShot >= m_FirePeriod) {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        m_TimeSinceLastShot = 0f;
+        Instantiate(m_BulletPrefab, m_SpawnPoint.position, m_SpawnPoint.rotation);
+    }
+}
