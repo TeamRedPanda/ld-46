e884e74cceb577dfedad51c991ac0b6410e9f963
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2020-04-19 19:30:07 -0300

    Add movement to the bullet

---
+++ Assets/Scripts/Bullet/BulletMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    [SerializeField] float m_Speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * m_Speed * Time.deltaTime;
+    }
+}
