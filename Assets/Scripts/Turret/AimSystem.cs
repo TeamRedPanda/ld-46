419414208af84d2309ef1a1c347a7e3095a3bf1e
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2020-04-19 16:43:19 -0300

    Turret aims at the player

---
+++ Assets/Scripts/Turret/AimSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimSystem : MonoBehaviour
+{
+    public Transform m_Target;
+    public Transform m_Pivot;
+
+    public float m_AngularSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Determine which direction to rotate towards
+        Vector3 targetDirection = m_Target.position - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float singleStep = m_AngularSpeed * Time.deltaTime;
+
+        // Rotate the forward vector towards the target direction by one step
+        Vector3 newDirection = Vector3.RotateTowards(m_Pivot.forward, targetDirection, singleStep, 0.0f);
+
+        // Draw a ray pointing at our target in
+        Debug.DrawRay(transform.position, newDirection, Color.red);
+
+        // Calculate a rotation a step closer to the target and applies rotation to this object
+        m_Pivot.rotation = Quaternion.LookRotation(newDirection);
+    }
+}
