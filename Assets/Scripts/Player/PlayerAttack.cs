1d53039693f0d0c0fe94a0790445569395b8ebd4
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2020-04-19 19:59:54 -0300

    Detect bat collision with bullet

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] Animator m_Animator;
+    [SerializeField] Collider m_HitCollider;
 
     public void Attack()
     {
@@ -14,6 +15,14 @@ public class PlayerAttack : MonoBehaviour
     public void Hit()
     {
         Debug.Log("Hit!");
+        var center = m_HitCollider.bounds.center;
+        var extents = m_HitCollider.bounds.extents;
+        var orientation = m_HitCollider.transform.rotation;
+        Collider[] objectsHit = Physics.OverlapBox(center, extents, orientation);
+
+        foreach (var objectHit in objectsHit) {
+            Debug.Log($"Hit {objectHit.name}");
+        }
     }
 
     public void BackswingFinished()
