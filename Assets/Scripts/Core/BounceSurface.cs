0e7c3f16207dc89d49bb864592dcdedda9d86741
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2020-04-19 21:55:09 -0300

    Make bullet bounce off walls

---
+++ Assets/Scripts/Core/BounceSurface.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceSurface : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        var currentDirection = other.transform.forward;
+        var normal = transform.right;
+        var newDirection = currentDirection - 2 * Vector3.Dot(normal, currentDirection) * normal;
+
+        other.transform.rotation = Quaternion.LookRotation(newDirection, Vector3.up);
+    }
+}
