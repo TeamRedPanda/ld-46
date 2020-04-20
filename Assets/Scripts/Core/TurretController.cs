756000ad0ca78f53300ecce4b74060c6780048bd
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2020-04-20 16:48:42 -0300

    Show turrets hit in HUD

---
+++ Assets/Scripts/Core/TurretController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    [SerializeField] CountView m_DestroyedTurretCountView;
+    int m_DestroyedTurretCount = 0;
+
+    public void OnTurretDestroyed()
+    {
+        m_DestroyedTurretCount++;
+        m_DestroyedTurretCountView.UpdateCount(m_DestroyedTurretCount);
+    }
+}
--- Assets/Scripts/Turret/Turret.cs
+++ Assets/Scripts/Turret/Turret.cs
@@ -10,8 +10,11 @@ public class Turret : MonoBehaviour
     [SerializeField] AudioSource m_AudioSource;
     [SerializeField] AudioClip m_BreakSound;
 
+
     public void Break()
     {
+        FindObjectOfType<TurretController>()?.OnTurretDestroyed();
+
         m_FiringController.StopFiring();
         m_AimSystem.StopAiming();
         m_AudioSource.PlayOneShot(m_BreakSound);
