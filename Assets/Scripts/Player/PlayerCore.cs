97b0ad4885f86e9813c95f357c2caf2fbbfbb9b7
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2020-04-20 16:10:50 -0300

    Import TextMesh pro essentials

--- Assets/Scripts/Player/PlayerCore.cs
+++ Assets/Scripts/Player/PlayerCore.cs
@@ -7,8 +7,11 @@ public class PlayerCore : MonoBehaviour
     [SerializeField] AudioSource m_AudioSource;
     [SerializeField] AudioClip m_HurtSound;
 
+    private int m_StrikeCount = 0;
+
     public void Damage()
     {
         m_AudioSource.PlayOneShot(m_HurtSound);
+        m_StrikeCount++;
     }
 }
